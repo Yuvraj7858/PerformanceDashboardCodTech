b9c7a69e91f27705a4ecb80c367f2e625af23189
webApp <yuvrajyuvi9939@gmail.com>
2025-08-13 11:08:17 +0530

    Add project files.

---
+++ PerformanceDashboard/Controllers/MetricsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace PerformanceDashboard.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MetricsController : ControllerBase
+    {
+        [HttpGet("status")]
+        public IActionResult GetStatus()
+        {
+            var process = Process.GetCurrentProcess();
+            var cpuUsage = GetCpuUsage();
+            var memoryUsage = process.WorkingSet64 / (1024 * 1024); // MB
+
+            return Ok(new
+            {
+                Message = "Application is running",
+                Time = DateTime.UtcNow,
+                CpuUsage = cpuUsage,
+                MemoryUsage = memoryUsage
+            });
+        }
+
+        private double GetCpuUsage()
+        {
+            using (var cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total"))
+            {
+                cpuCounter.NextValue();
+                Thread.Sleep(500); // थोड़ा wait जरूरी है accurate reading के लिए
+                return Math.Round(cpuCounter.NextValue(), 2);
+            }
+        }
+    }
+}
